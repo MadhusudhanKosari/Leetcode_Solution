16e99acacf19b8491cb73113aae86d76741429e0
Madhusudhan Kosari <23211a05e8@bvrit.ac.in>
2025-12-09 22:55:10 +0530

    Time: 0 ms (100%), Space: 43 MB (13.56%) - LeetHub

--- 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.java
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.java
@@ -1,10 +1,19 @@
 class Solution {
     public char repeatedCharacter(String s) {
-        HashMap<Character,Integer> hm = new HashMap<>();
-        //char arr[] = s.toCharArray();
+        // HashMap<Character,Integer> hm = new HashMap<>();
+        // //char arr[] = s.toCharArray();
+        // for(int i = 0;i<s.length();i++){
+        //     hm.put(s.charAt(i),hm.getOrDefault(s.charAt(i),0)+1);
+        //     if(hm.get(s.charAt(i)) == 2){
+        //         return s.charAt(i);
+        //     }
+        // }
+        // return ' ';
+        int arr[] = new int[26];
         for(int i = 0;i<s.length();i++){
-            hm.put(s.charAt(i),hm.getOrDefault(s.charAt(i),0)+1);
-            if(hm.get(s.charAt(i)) == 2){
+            int idx = s.charAt(i)-'a';
+            arr[idx]++;
+            if(arr[idx] == 2){
                 return s.charAt(i);
             }
         }
