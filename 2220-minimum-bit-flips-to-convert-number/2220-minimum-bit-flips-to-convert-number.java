1465466ca82ee0585463b0afea99b34670985368
Madhusudhan Kosari <23211a05e8@bvrit.ac.in>
2026-01-26 11:17:25 +0530

    Time: 0 ms (100%), Space: 42.4 MB (24.52%) - LeetHub

--- 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.java
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.java
@@ -3,8 +3,9 @@ class Solution {
         int num = start^goal;
         int count = 0;
         while(num>0){
-            if((num&1) == 1) count++;
-            num = num>>1;
+            // if((num&1) == 1) count++;
+            num = num&(num-1);
+            count++;
         }
         return count;
     }
