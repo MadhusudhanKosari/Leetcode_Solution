2aff698cc170561c13870fd89a6ab3625c7d6821
Madhusudhan Kosari <23211a05e8@bvrit.ac.in>
2026-01-30 19:51:38 +0530

    Time: 4 ms (73.26%), Space: 45.4 MB (29.85%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,54 +1,25 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int res[] = new int[nums1.length];
-        int k = 0;
-        // HashMap<Integer,Integer> hm = new HashMap<>();
-        
-        // for(int i = 0;i<nums2.length;i++){
-        //     hm.put(nums2[i],i);
-        // }
-        // boolean flag = true;
-        // for(int num : nums1){
-        //     int idx = hm.get(num);
-        //     for(int j = idx+1;j<nums2.length;j++){
-        //         if(num < nums2[j]){
-        //             res[k++] = nums2[j];
-        //             flag = false;
-        //             break;
-        //         }
-        //     }
-        //     if(flag){
-        //         res[k++] = -1;
-        //     }else{
-
-        //         flag = true;
-        //     }
-        // }
-        // return res;
-        Stack<Integer> st1 = new Stack<>();
-        Stack<Integer> st2 = new Stack<>();
-        for(int num : nums2){
-            st1.push(num);
+       Stack<Integer> st = new Stack<>();
+       HashMap<Integer,Integer> hm = new HashMap<>();
+       int n = nums2.length;
+       for(int i = n-1;i>=0;i--){
+        while(!st.isEmpty() && st.peek() <= nums2[i]){
+            st.pop();
         }
-        for(int i = 0;i<nums1.length;i++){
-            int maxi = Integer.MIN_VALUE;
-            while(!st1.isEmpty() && st1.peek() != nums1[i]){
-                // maxi = Math.max(maxi,st1.peek());
-                if(st1.peek()>nums1[i]){
-                    maxi = st1.peek();
-                }
-                st2.push(st1.pop());
-            }
-            if(maxi != Integer.MIN_VALUE && maxi > nums1[i]){
-                res[k++] = maxi;
-            }
-            else{
-                res[k++] = -1;
-            }
-            while(!st2.isEmpty()){
-                st1.push(st2.pop());
-            }
+        if(st.isEmpty()){
+            hm.put(nums2[i],-1);
         }
-        return res;
+        else{
+            hm.put(nums2[i],st.peek());
+        }
+        st.push(nums2[i]);
+       }
+       int res[] = new int[nums1.length];
+       int  k = 0;
+       for(int num : nums1){
+        res[k++] = hm.get(num);
+       }
+       return res;
     }
 }
